7136ba478e6e728835c6c326f5c43448b12ef683
Ayush <ayush15may@gmail.com>
2021-02-05 00:19:17 +0530

    fix: update expression

--- functions/products/utils/flatupdateParams.js
+++ functions/products/utils/flatupdateParams.js
@@ -1,15 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const flatUpdateParams = (params) => ({
-    UpdateExpression: `SET ${Object.entries(params)
-        .filter(([key, value]) => value !== null)
-        .map(([key]) => `#${key} = :${key}, `)
-        .reduce((acc, str) => acc + str)
-        .slice(0, -2)} REMOVE ${Object.entries(params) // This is to remove trailing comma and space
-        .filter(([key, value]) => value === null)
-        .map(([key]) => `${key} `)
-        .reduce((acc, str) => acc + str)
-        .slice(0, -1)}`,
+    UpdateExpression: Object.values(params).includes(null) ? withNull(params) : withoutNull(params),
     ExpressionAttributeNames: Object.entries(params)
         .filter(([key, value]) => value !== null)
         .map(([key, value]) => key)
@@ -24,4 +16,18 @@ const flatUpdateParams = (params) => ({
         [`:${key}`]: value,
     }), {}),
 });
+const withNull = (params) => `SET ${Object.entries(params)
+    .filter(([key, value]) => value !== null)
+    .map(([key]) => `#${key} = :${key}, `)
+    .reduce((acc, str) => acc + str)
+    .slice(0, -2)} REMOVE ${Object.entries(params) // This is to remove trailing comma and space
+    .filter(([key, value]) => value === null)
+    .map(([key]) => `${key} `)
+    .reduce((acc, str) => acc + str, '')
+    .slice(0, -1)}`;
+const withoutNull = (params) => `SET ${Object.entries(params)
+    .filter(([key, value]) => value !== null)
+    .map(([key]) => `#${key} = :${key}, `)
+    .reduce((acc, str) => acc + str, '')
+    .slice(0, -2)}`;
 exports.default = flatUpdateParams;
--- src/products/utils/flatupdateParams.ts
+++ src/products/utils/flatupdateParams.ts
@@ -1,13 +1,5 @@
 const flatUpdateParams = (params: any) => ({
-  UpdateExpression: `SET ${Object.entries(params)
-    .filter(([key, value]) => value !== null)
-    .map(([key]) => `#${key} = :${key}, `)
-    .reduce((acc, str) => acc + str)
-    .slice(0, -2)} REMOVE ${Object.entries(params) // This is to remove trailing comma and space
-    .filter(([key, value]) => value === null)
-    .map(([key]) => `${key} `)
-    .reduce((acc, str) => acc + str)
-    .slice(0, -1)}`,
+  UpdateExpression: Object.values(params).includes(null) ? withNull(params) : withoutNull(params),
   ExpressionAttributeNames: Object.entries(params)
     .filter(([key, value]) => value !== null)
     .map(([key, value]) => key)
@@ -28,5 +20,23 @@ const flatUpdateParams = (params: any) => ({
       {}
     ),
 })
+
+const withNull = (params: any) =>
+  `SET ${Object.entries(params)
+    .filter(([key, value]) => value !== null)
+    .map(([key]) => `#${key} = :${key}, `)
+    .reduce((acc, str) => acc + str)
+    .slice(0, -2)} REMOVE ${Object.entries(params) // This is to remove trailing comma and space
+    .filter(([key, value]) => value === null)
+    .map(([key]) => `${key} `)
+    .reduce((acc, str) => acc + str, '')
+    .slice(0, -1)}`
+
+const withoutNull = (params: any) =>
+  `SET ${Object.entries(params)
+    .filter(([key, value]) => value !== null)
+    .map(([key]) => `#${key} = :${key}, `)
+    .reduce((acc, str) => acc + str, '')
+    .slice(0, -2)}`
 
 export default flatUpdateParams
